f04017a2f541953907fcf987b4a5bf351f1c9069
Tarciso Torres <tarcisoftorres@gmail.com>
2021-01-27 16:17:00 -0300

    chore: valida o token

--- src/main/java/br/com/alura/forum/config/security/TokenService.java
+++ src/main/java/br/com/alura/forum/config/security/TokenService.java
@@ -35,4 +35,14 @@ public class TokenService {
 				.compact();
 	}
 
+
+	public Boolean isTokenValido(String token) {
+		try {			
+			Jwts.parser().setSigningKey(this.secret).parseClaimsJws(token);
+			return true;
+		}catch(Exception e) {
+			return false;
+		}
+	}
+
 }
